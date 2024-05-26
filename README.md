e54dee12ee89e32ef5861d5148ee2a92cbd73490
ptrbelonovsky <p.belonovsky@gmail.com>
2024-05-26 15:39:09 +0300

    data parsing comments

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-### This repository contains the code for the thesis "The estimation of Value-at-Risk and Expected Shortfall based on deep generative models"
+## This repository contains the code for the thesis "The estimation of Value-at-Risk and Expected Shortfall based on deep generative models"
+---
 Author:  **Belonovskiy Peter Ilich, HSE DSBA** 
 
 Supervisor: **Naumenko Vladimir Vladimirovich, HSE Associate Professor**
 
+### Installation
+---
+The dependency managament in project was implemented via [poetry](https://python-poetry.org).
+To clone this repository and set up the environment, run the following commands:
+```bash
+git clone https://github.com/BELONOVSKII/var_es_dgm.git
+cd var_es_dgm
+poetry install
+poetry shell
+```
+Note, poetry should be pre-installed in your system.
+
+### Download data
+---
+Thesis uses daily stock prices data from yahoo finance. To parse the yahoo finance and download data run:
+```python
+python var_es_dgm/data_parcing/parse_yfinance.py 
+```
+This downloads individual stocks's data and produces combined file `data/complete_stocks.csv` that would be further used in the experiments.
--- var_es_dgm/data_parcing/parse_yfinance.py
+++ var_es_dgm/data_parcing/parse_yfinance.py
@@ -3,55 +3,91 @@ import yfinance as yf
 import pandas as pd
 
 
-# Define a function to download data for a single stock
 def download_stock_data(ticker, start_date, end_date):
     """
-    Downloads historical stock data for a given ticker symbol and date range.
+    Download historical stock data for a given ticker within a specified date range.
 
-    Args:
-        ticker (str): Ticker symbol of the stock.
-        start_date (str): Start date in YYYY-MM-DD format.
-        end_date (str): End date in YYYY-MM-DD format.
+    Parameters
+    ----------
+    ticker : str
+        Stock ticker symbol.
+    start_date : str
+        Start date in the format 'YYYY-MM-DD'.
+    end_date : str
+        End date in the format 'YYYY-MM-DD'.
 
-    Returns:
-        pandas.DataFrame: DataFrame containing historical stock data.
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing the stock data.
     """
     return yf.download(ticker, start=start_date, end=end_date)
 
 
 def parce_data(tickers, start_date, end_date, output_path=None):
+    """
+    Download and save stock data for multiple tickers.
+
+    Parameters
+    ----------
+    tickers : list
+        List of stock ticker symbols.
+    start_date : str
+        Start date in the format 'YYYY-MM-DD'.
+    end_date : str
+        End date in the format 'YYYY-MM-DD'.
+    output_path : str, optional
+        Directory path to save the CSV files. Defaults to None.
+
+    Returns
+    -------
+    None
+    """
     stock_data = {}
     for ticker in tickers:
         try:
+            # Download stock data and reset index
             stock_data[ticker] = download_stock_data(
                 ticker, start_date, end_date
             ).reset_index()
+            # Save data to CSV if output_path is provided
             if output_path:
                 stock_data[ticker].to_csv(
                     output_path + ticker.replace(".", "_") + ".csv", index=False
                 )
             print(ticker, stock_data[ticker]["Date"].min())
         except (yf.DownloadError, KeyError):
+            # Handle download errors
             print(f"Error downloading data for {ticker}")
 
 
 def take_trading_from_2005():
+    """
+    Filter stocks with trading data starting from 2005 and save the combined data to a CSV file.
+
+    Returns
+    -------
+    None
+    """
     PATH_TO_DATA = "data/stocks/"
     data_files = list(filter(lambda x: x[-4:] == ".csv", os.listdir(PATH_TO_DATA)))
     df = pd.DataFrame()
     for file_name in data_files:
+        # Read CSV files and add Ticker column
         temp = pd.read_csv(PATH_TO_DATA + file_name, parse_dates=["Date"]).assign(
             Ticker=file_name[:-4]
         )
-
         try:
+            # Concatenate data if trading started on or before 2005
             if temp["Date"].min().year <= 2005:
                 df = pd.concat([df, temp])
         except AttributeError:
             continue
 
+    # Print number of suitable stocks and list of companies
     print("Number of suitable stocks: {0}".format(df["Ticker"].nunique()))
     print("\nList of availiable companies:\n", df["Ticker"].unique())
+    # Save the combined data to a CSV file
     df.to_csv("data/complete_stocks.csv", index=False)
 
 
@@ -68,5 +104,7 @@ if __name__ == "__main__":
     # Define start and end date (change as needed)
     start_date = "2005-01-01"
     end_date = "2023-12-31"
+    # Download and save stock data
     parce_data(tickers, start_date, end_date, output_path=DATA_FOLDER + "stocks/")
+    # Filter and save trading data from 2005
     take_trading_from_2005()
